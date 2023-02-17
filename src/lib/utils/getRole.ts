bfa474d7d29c6ec25d957310ed0e46c0a8af3713
WT000 <55751558+WT000@users.noreply.github.com>
2023-02-17 17:25:05 +0000

    getRole func

--- src/lib/utils/getRole.ts
+++ src/lib/utils/getRole.ts
@@ -1,3 +1,22 @@
 import Role from "./roles";
+import { HomeInterface } from "@/db/models/Home";
+import { UserInterface } from "@/db/models/User";
 
 // Takes in a Home and User, returns an enum based on the type
+function getRole(home: HomeInterface, user: UserInterface) {
+    // Check if Agency (admin)
+    if (user.isAgency) return Role.Agency;
+
+    // Check if Homeowner (owns the home)
+    if (home.owner.toString() == user._id.toString()) return Role.Homeowner;
+
+    // Check if Delegate (worker of the home)
+    home.delegates.forEach(delegate => {
+        if (delegate.toString() == user._id.toString()) return Role.Delegate;
+    });
+
+    // Assume user is Guest (other types of user)
+    return Role.Guest;
+}
+
+export default getRole;
