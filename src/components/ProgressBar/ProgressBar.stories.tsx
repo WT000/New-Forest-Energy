167f39eb196ee58d5a284f8ed3533008aeb93ceb
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-02-22 14:50:41 +0000

    Added storybook to display progress bar

---
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,19 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import ProgressBar from "./ProgressBar";
+
+
+export default {
+    title: "ProgressBar",
+    component: ProgressBar,
+} as ComponentMeta<typeof ProgressBar>;
+  
+const Template: ComponentStory<typeof ProgressBar> = (args) => <ProgressBar {...args} />;
+  
+export const Bar1 = Template.bind({});
+
+Bar1.args = {
+    smallnum: 8.34,
+    largenum: 10.36,
+
+};
+
