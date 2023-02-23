492badabae9d299039174462337d6e7b9a822149
jorandall <5randj17@solent.ac.uk>
2023-02-23 11:41:59 +0000

    updated test page

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -9,10 +9,32 @@ import {IoHome, IoPieChart, IoFlash, IoCalendar, IoList, IoLogOut, IoAdd} from "
 export default function test() {
     return (
         <div>
-            <div className= "hidden md:block">
-                <DesktopNavbar 
-                text="Welcome back, Rachel"
-                image="/stories/rachel.jpeg"
+            <DesktopNavbar 
+            text="Welcome back, Rachel"
+            image="/stories/rachel.jpeg"
+            menu={
+                <NavbarMenu>
+                    <NavbarMenuItem
+                        icon={<IoHome />}
+                        text="All Homes"
+                        onClick={() => console.log("AllHomes")}
+                        activePage={false} 
+                    />
+                    <NavbarMenuItem
+                        icon={<IoPieChart />}
+                        text="Dashboard"
+                        onClick={() => console.log("Dashboard")}
+                        activePage={true} 
+                    />
+                </NavbarMenu>  
+                }
+            >
+                <NavbarStats stat="30" text="some stats (testing)" />
+                <NavbarStats stat="£4.50" text="cost"/>
+                <NavbarStats stat="60" text="some more stats (that are cool)"/>
+            </DesktopNavbar>
+        
+            <MobileNavbar 
                 menu={
                     <NavbarMenu>
                         <NavbarMenuItem
@@ -27,34 +49,10 @@ export default function test() {
                             onClick={() => console.log("Dashboard")}
                             activePage={true} 
                         />
-                    </NavbarMenu>  
-                    }
-                >
-                    <NavbarStats stat="30" text="some stats (testing)" />
-                    <NavbarStats stat="£4.50" text="cost"/>
-                    <NavbarStats stat="60" text="some more stats (that are cool)"/>
-                </DesktopNavbar>
-            </div>
-            <div className="md:hidden">
-                <MobileNavbar 
-                    menu={
-                        <NavbarMenu>
-                            <NavbarMenuItem
-                                icon={<IoHome />}
-                                text="All Homes"
-                                onClick={() => console.log("AllHomes")}
-                                activePage={false} 
-                            />
-                            <NavbarMenuItem
-                                icon={<IoPieChart />}
-                                text="Dashboard"
-                                onClick={() => console.log("Dashboard")}
-                                activePage={true} 
-                            />
-                            </NavbarMenu>
-                    } 
-                />
-            </div>
+                        </NavbarMenu>
+                } 
+            />
+        
         
     </div>
     )
