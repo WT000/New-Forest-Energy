846d84f9534f6d307f22d23bcf4bedad58071a8d
jorandall <5randj17@solent.ac.uk>
2023-02-23 10:20:44 +0000

    corrected indents and removed unused import

--- src/components/DesktopNavbar/DesktopNavbar.tsx
+++ src/components/DesktopNavbar/DesktopNavbar.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image"
-import NavbarMenu from "../NavbarMenu/NavbarMenu";
-
 
 interface DesktopNavbarProps {
     text: string;
@@ -24,28 +22,27 @@ export default function DesktopNavbar(props: DesktopNavbarProps) {
     return (
         <div className="w-[260px] min-h-full m-4 rounded-[20px] shadow-[0_4px_100px_rgba(0,0,0,0.1)] grid grid-cols-1 py-9">
             <div>
-            <Image
-                className="m-auto pb-12"
-                src="/tmp-title.PNG"
-                alt="Logo"
-                width={120}
-                height={31}
-                unoptimized={true}
-            />
-            <div className="m-auto pb-4">
-                <div className="flex items-center justify-center pb-2">
-                    <Image
-                        className="rounded-full h-24 w-24"
-                        src={image}
-                        alt="Profile Picture"
-                        width={193}
-                        height={108}
-                        unoptimized={true}
-                        
-                    />
+                <Image
+                    className="m-auto pb-12"
+                    src="/tmp-title.PNG"
+                    alt="Logo"
+                    width={120}
+                    height={31}
+                    unoptimized={true}
+                />
+                <div className="m-auto pb-4">
+                    <div className="flex items-center justify-center pb-2">
+                        <Image
+                            className="rounded-full h-24 w-24"
+                            src={image}
+                            alt="Profile Picture"
+                            width={193}
+                            height={108}
+                            unoptimized={true}
+                            
+                        />
+                    </div>
                 </div>
-            </div>
-            
                 <div className="text-center">
                     <span className="text-sm text-[#77767A]">{welcome},</span><br/>
                     <span className="font-bold text-lg">{name}</span>
@@ -54,14 +51,10 @@ export default function DesktopNavbar(props: DesktopNavbarProps) {
             <div>
                 {children}
             </div>
-                
-            
             <div className="m-auto ">
                 <div className="h-[0px] w-[173px] mx-auto border-b-2 border-[#1D1A221A]"></div>
                 {menu}
             </div>
-            
         </ div>
     )
-
 }
