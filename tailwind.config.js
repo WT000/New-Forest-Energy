f15e69e0fdb5c0ae7d80d237554ecee59dd17aaa
WT000 <55751558+WT000@users.noreply.github.com>
2023-02-24 00:08:15 +0000

    Fix formatting from merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,41 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    screens: {
-      "xs": "500px"
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        screens: {
+            xs: "500px",
+        },
+        extend: {},
+        colors: {
+            orange: "#F58800",
+            yellow: "#F8BC24",
+            green: {
+                400: "#266867",
+                500: "#1A4645",
+            },
+            black: {
+                DEFAULT: "#051821",
+                500: "#77767A",
+            },
+            white: {
+                DEFAULT: "#FFFFFF",
+                100: "#FAFAFA",
+            },
+        },
+        extend: {
+            fontFamily: {
+                sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    extend: {},
-  colors: {
-      'orange': '#F58800',
-      'yellow': '#F8BC24',
-      'green': {
-        400: '#266867',
-        500: '#1A4645',
-      },
-      'black': {
-        DEFAULT: '#051821',
-        500: '#77767A'
-      },
-      'white': {
-        DEFAULT: '#FFFFFF',
-        100: '#FAFAFA'
-      },
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
