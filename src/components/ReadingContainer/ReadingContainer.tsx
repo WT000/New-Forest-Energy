49b46895e9bd7350381055e33fbc8e16193e373b
Steven Hawkins <Steven Hawkins@PTNOWSA6000378>
2023-02-24 15:16:13 +0000

    Modified scroll to be hidden and draggable

--- src/components/ReadingContainer/ReadingContainer.tsx
+++ src/components/ReadingContainer/ReadingContainer.tsx
@@ -1,83 +1,114 @@
 import React, { useState } from "react";
 import Reading from "../Reading/Reading";
-import useInfiniteScroll, { ScrollDirectionBooleanState, ScrollDirection } from "react-easy-infinite-scroll-hook";
+import useInfiniteScroll, {
+	ScrollDirectionBooleanState,
+	ScrollDirection,
+} from "react-easy-infinite-scroll-hook";
+import { useDraggable } from "react-use-draggable-scroll";
+import { useRef } from "react";
+import useMergedRef from "@react-hook/merged-ref";
 
-function loadMore(setCurrentOffset: (number) => void, currentOffset: number, offset: number, data: []) {
-    const startingIndex = currentOffset;
-    const newOffset = currentOffset + offset;
+function loadMore(
+	setCurrentOffset: (number) => void,
+	currentOffset: number,
+	offset: number,
+	data: []
+) {
+	const startingIndex = currentOffset;
+	const newOffset = currentOffset + offset;
 
-    setCurrentOffset(newOffset);
-    return data.slice(startingIndex, newOffset);
+	setCurrentOffset(newOffset);
+	return data.slice(startingIndex, newOffset);
 }
 
 const createNext =
-    ({
-        setData,
-        setCurrentOffset,
-        offset,
-        currentOffset,
-        data,
-    }: {
-        setData: (v: React.SetStateAction<string[]>) => void;
-        setCurrentOffset: (v: React.SetStateAction<number>) => void;
-        offset: number;
-        currentOffset: number;
-        data: [];
-    }) =>
-    async () => {
-        try {
-            const rows = loadMore(setCurrentOffset, currentOffset, offset, data);
+	({
+		setData,
+		setCurrentOffset,
+		offset,
+		currentOffset,
+		data,
+	}: {
+		setData: (v: React.SetStateAction<string[]>) => void;
+		setCurrentOffset: (v: React.SetStateAction<number>) => void;
+		offset: number;
+		currentOffset: number;
+		data: [];
+	}) =>
+	async () => {
+		try {
+			const rows = loadMore(
+				setCurrentOffset,
+				currentOffset,
+				offset,
+				data
+			);
 
-            setData((prev) => [...rows, ...prev]);
-        } finally {
-        }
-    };
+			setData((prev) => [...rows, ...prev]);
+		} finally {
+		}
+	};
 
 export interface ReadingContainertInterface {
-    readings: [];
+	readings: [];
 }
 
 export default function ReadingContainer(props: ReadingContainertInterface) {
-    // Setdata SHOULD BE SETTING READING VALUES
-    const { readings } = props;
+	// Setdata SHOULD BE SETTING READING VALUES
+	const { readings } = props;
 
-    const [offset, setCurrentOffset] = useState(6);
+	const [offset, setCurrentOffset] = useState(6);
 
-    const [data, setData] = useState(readings ? readings.slice(0, 6) : []);
-    const [hasMore, setHasMore] = useState<ScrollDirectionBooleanState>({
-        up: false,
-        down: true,
-    });
+	const [data, setData] = useState(readings ? readings.slice(0, 6) : []);
+	const [hasMore, setHasMore] = useState<ScrollDirectionBooleanState>({
+		up: false,
+		down: true,
+	});
 
-    const ref = useInfiniteScroll<HTMLDivElement>({
-        next: createNext({ setData, setCurrentOffset, offset: 6, currentOffset: offset, data: readings }),
-        rowCount: readings.length,
-        hasMore,
-    });
-    
-    return (
-        <div>
-            <div
-                ref={ref}
-                className="List bg-white-100 h-[35vh] w-[60vw] overflow-y-auto flex flex-col"
-            >
-                {data.map((reading, index) => (
-                    <Reading
-                        key={index}
-                        //@ts-ignore
-                        creator={reading.user?.name ? reading.user.name : "Guest"}
-                        //@ts-ignore
-                        kwhValue={reading.value}
-                        //@ts-ignore
-                        image={reading.image}
-                        //@ts-ignore
-                        createdAt={new Date(reading.createdAt)}
-                        onClick={() => {
-                            console.log("click");
-                        }}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+	const ref = useInfiniteScroll<HTMLDivElement>({
+		next: createNext({
+			setData,
+			setCurrentOffset,
+			offset: 6,
+			currentOffset: offset,
+			data: readings,
+		}),
+		rowCount: readings.length,
+		hasMore,
+	});
+
+	const drag =
+		useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+	const { events } = useDraggable(drag); // Now we pass the reference to the useDraggable hook:
+
+	const multiRef = useMergedRef(ref, drag);
+
+	return (
+		<div>
+			<div
+				ref={multiRef}
+				{...events}
+				className="List bg-white-100 h-[35vh] w-[60vw] overflow-y-auto flex flex-col scrollbar-hide"
+			>
+				{data.map((reading, index) => (
+					<Reading
+						key={index}
+						//@ts-ignore
+						creator={
+							reading.user?.name ? reading.user.name : "Guest"
+						}
+						//@ts-ignore
+						kwhValue={reading.value}
+						//@ts-ignore
+						image={reading.image}
+						//@ts-ignore
+						createdAt={new Date(reading.createdAt)}
+						onClick={() => {
+							console.log("click");
+						}}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,40 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
 
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        // screens: {
-        //     xs: "500px",
-        // },
-        extend: {},
-        colors: {
-            orange: "#F58800",
-            yellow: "#F8BC24",
-            green: {
-                400: "#266867",
-                500: "#1A4645",
-            },
-            black: {
-                DEFAULT: "#051821",
-                500: "#77767A",
-            },
-            white: {
-                DEFAULT: "#FFFFFF",
-                100: "#FAFAFA",
-            },
-        },
-        extend: {
-            fontFamily: {
-                sans: ["Roboto", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-    plugins: [],
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		screens: {
+			xs: "500px",
+		},
+		extend: {},
+		colors: {
+			orange: "#F58800",
+			yellow: "#F8BC24",
+			green: {
+				400: "#266867",
+				500: "#1A4645",
+			},
+			black: {
+				DEFAULT: "#051821",
+				500: "#77767A",
+			},
+			white: {
+				DEFAULT: "#FFFFFF",
+				100: "#FAFAFA",
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require("tailwind-scrollbar-hide")],
 };
