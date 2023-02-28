27d8398d0cad0eb7222ed9338e729e6343b4b10d
jorandall <5randj17@solent.ac.uk>
2023-02-28 14:05:43 +0000

    fixed sizing bug

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -26,7 +26,7 @@ export default function Card(props: CardProps) {
     if(cardType == CardType.stats) {styling = "h-20 w-24 shadow-[0_2px_10px_rgba(0,0,0,0.1)] rounded-[5px]"}
     if(cardType == CardType.icon) {styling = "h-8 w-8 md:w-10 md:h-10 shadow-[0_4px_10px_rgba(0,0,0,0.1)] rounded-lg"}
     if(cardType == CardType.comparison) {styling = "h-[46px] w-[150px] rounded-[5px] shadow-[0_2px_10px_rgba(0,0,0,0.1)] md:h-[77px] md:w-[176px] md:rounded-[10px] md:shadow-[0_4px_20px_rgba(0,0,0,0.1)]"}
-    if(cardType == CardType.booking) {styling = "h-[170px] w-[170px] shadow-[0_4px_10px_rgba(0,0,0,0.1)] rounded-[10px] w-full"}
+    if(cardType == CardType.booking) {styling = "h-[170px] w-[170px] shadow-[0_4px_10px_rgba(0,0,0,0.1)] rounded-[10px]"}
     if(cardType == CardType.colourThumbnail) {styling = "h-4 w-4 shadow-[0_2px_1px_rgba(0,0,0,0.1)] rounded-[5px]"}
 
     let bgColour = "bg-white"
