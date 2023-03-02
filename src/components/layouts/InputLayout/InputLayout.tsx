6bb61fb197a5afc8b8bb6d9e33a2d689aa739ce6
jorandall <5randj17@solent.ac.uk>
2023-03-02 14:06:19 +0000

    changed bg colour to transparent

--- src/components/layouts/InputLayout/InputLayout.tsx
+++ src/components/layouts/InputLayout/InputLayout.tsx
@@ -19,8 +19,8 @@ export default function InputLayout(props: InputLayoutProps) {
                 <p className="text-black-500 text-xs">{text}</p>
                 <p className="">
                     <input 
-                        className="text-lg placeholder:text-black font-bold bg-white-100 w-[95%] 
-                            file:border-none file:bg-white-100 file:cursor-pointer :" 
+                        className="text-lg placeholder:text-black font-bold bg-transparent w-[95%] 
+                            file:border-none file:bg-transparent file:cursor-pointer :" 
                         type={type} 
                         name={name} 
                         placeholder={placeholder}
