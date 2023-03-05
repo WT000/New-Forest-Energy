49958815c423048d03a3d6567eb71bf4ac9cbc91
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-03-05 04:30:57 +0000

    Created showbar prop

--- src/components/subtitles/Subtitles.stories.tsx
+++ src/components/subtitles/Subtitles.stories.tsx
@@ -10,10 +10,18 @@ const Template: ComponentStory<typeof Subtitles> = (args) => (
   <Subtitles {...args} />
 );
 
-export const Bar1 = Template.bind({});
+export const SubtitlesWithBar = Template.bind({});
 
-Bar1.args = {
+SubtitlesWithBar.args = {
     text1: "Average Usage Per Day",
-    text2: "Secondary Text"
+    text2: "Secondary Text",
+    showbar: true
 };
 
+export const SubtitlesWithoutBar = Template.bind({});
+
+SubtitlesWithoutBar.args = {
+    text1: "Average Usage Per Day",
+    text2: "Secondary Text",
+    showbar: false
+};
