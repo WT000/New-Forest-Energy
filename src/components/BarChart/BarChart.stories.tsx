089d7316f102c6f3f04fc6d6794e262e36327679
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-03-06 22:11:12 +0000

    Populated storybook data dictionary

--- src/components/BarChart/BarChart.stories.tsx
+++ src/components/BarChart/BarChart.stories.tsx
@@ -15,6 +15,32 @@ export const Bar1 = Template.bind({});
 
 
 
+Bar1.args = {
+
+    datalist: [
+        {
+            num: 27,
+            date: '23'
+        },
+        {
+            num: 28,
+            date: '24'
+        },
+        {
+            num: 31,
+            date: '25'
+        },
+        {
+            num: 19,
+            date: '26'
+        }
+    ],
+
+    daty: [27,28,29,30,31,1],
+    numby: [5, 6, 8, 3, 2, 6]
+
+};
+
 
 
 
