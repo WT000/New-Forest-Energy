deaac391442c063aac327e9d7813850ae00bf03e
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-03-06 22:24:09 +0000

    Formatted code

--- src/components/BarChart/BarChart.stories.tsx
+++ src/components/BarChart/BarChart.stories.tsx
@@ -1,46 +1,34 @@
-import {ComponentMeta, ComponentStory} from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import BarChart from "./BarChart";
 
-
 export default {
-    title: "BarChart",
-    component: BarChart,
+  title: "BarChart",
+  component: BarChart,
 } as ComponentMeta<typeof BarChart>;
-  
-const Template: ComponentStory<typeof BarChart> = (args) => <BarChart {...args}/>;
 
-  
+const Template: ComponentStory<typeof BarChart> = (args) => (
+  <BarChart {...args} />
+);
+
 export const Bar1 = Template.bind({});
 
-
-
-
 Bar1.args = {
-
-    datalist: [
-        {
-            num: 27,
-            date: '23 November'
-        },
-        {
-            num: 28,
-            date: '24 November'
-        },
-        {
-            num: 31.5,
-            date: '25 November'
-        },
-        {
-            num: 19,
-            date: '26 November'
-        }
-    ],
-
-
-
+  datalist: [
+    {
+      num: 27,
+      date: "23 November",
+    },
+    {
+      num: 28,
+      date: "24 November",
+    },
+    {
+      num: 31.5,
+      date: "25 November",
+    },
+    {
+      num: 19,
+      date: "26 November",
+    },
+  ],
 };
-
-
-
-
-
--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -5,92 +5,98 @@
 // import ToolTip from 'chart.js'
 // import Legend from 'chart.js'
 
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 import { Bar } from "react-chartjs-2";
 
 import type { ChartData, ChartOptions } from "chart.js";
 
 interface ChartProps {
-    options: ChartOptions<"line">;
-    data: ChartData<"line">;
-    datalist: { num: number, date: string}
+  options: ChartOptions<"line">;
+  data: ChartData<"line">;
+  datalist: { num: number; date: string };
 }
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 export default function BarChart(props: ChartProps) {
-    const {datalist} = props;
+  const { datalist } = props;
 
-    const datalistd = []
-    for (var val in datalist){
-        let value = datalist[val]['date']
-        datalistd.push(value)
-    }
+  const datalistd = [];
+  for (var val in datalist) {
+    let value = datalist[val]["date"];
+    datalistd.push(value);
+  }
 
-    const datalistn = []
-    for (var val in datalist){
-        let value = datalist[val]['num']
-        datalistn.push(value)
-    }
+  const datalistn = [];
+  for (var val in datalist) {
+    let value = datalist[val]["num"];
+    datalistn.push(value);
+  }
 
-    const data = {
-        labels: datalistd,
-        datasets: [
-            {
+  const data = {
+    labels: datalistd,
+    datasets: [
+      {
+        data: datalistn,
+        backgroundColor: "#266867",
+        borderRadius: 10,
+        borderSkipped: false,
+        barPercentage: 0.5,
+      },
+    ],
+  };
 
-                data: datalistn,
-                backgroundColor: "#266867",
-                borderRadius: 10,
-                borderSkipped: false,
-                barPercentage: 0.5,
-            },
-        ],
-    };
+  const options = {
+    maintainAspectRatio: true,
 
-    const options = {
-        maintainAspectRatio: true,
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          fontColor: "blue",
+          fontSize: 70,
+        },
+      },
+    },
 
-        plugins: {
-            legend: {
-                display: false,
-                labels: {
-                    fontColor: "blue",
-                    fontSize: 70,
-                },
-            },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        border: {
+          display: false,
         },
 
-        scales: {
-            x: {
-                grid: {
-                    display: false,
-                },
-                border: {
-                    display: false,
-                },
-
-                ticks: { color: "#77767A" },
-            },
-            y: {
-                grid: {
-                    lineWidth: "4.5",
-                    color: "#EDEEF0",
-                },
-                border: {
-                    display: false,
-                },
-
-                ticks: { color: "#77767A" },
-            },
+        ticks: { color: "#77767A" },
+      },
+      y: {
+        grid: {
+          lineWidth: "4.5",
+          color: "#EDEEF0",
         },
-    };
+        border: {
+          display: false,
+        },
 
-    return (
-        <div>
-            <div style={{ padding: "20px", width: "50%" }}>
-                <Bar data={data} options={options}></Bar>
-            </div>
-        </div>
-    );
+        ticks: { color: "#77767A" },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <div style={{ padding: "20px", width: "50%" }}>
+        <Bar data={data} options={options}></Bar>
+      </div>
+    </div>
+  );
 }
