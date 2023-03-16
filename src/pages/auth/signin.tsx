25b80d5496fbdbe25d3f75305df3463f1b487030
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-16 16:40:06 +0000

    Added svg to login page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -2,18 +2,45 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "nex
 import { getProviders, signIn } from "next-auth/react"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]";
+import Image from 'next/image';
+
 
 export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
+    <div className="grid md:grid-cols-2 min-h-screen">
+      <div>
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+      
+      <div className="hidden md:block w-full relative -z-10">
+        <Image
+          className="bottom-8"
+          priority
+          src="/img/leafBackgroundVertical.svg"
+          fill
+          alt="Leaf Image"
+          />
+      </div>
+
+      {/*<div className="hidden md:block h-32 overflow-clip">
+          <div className="h-56 w-full relative -z-10 bottom-8">
+              <Image
+                  priority
+                  src="/img/leafBackgroundVertical.svg"
+                  fill
+                  alt="Leaf Image"
+                  />
+          </div>                    
+        </div>*/}
+
+      
+    </div>
   )
 }
 
