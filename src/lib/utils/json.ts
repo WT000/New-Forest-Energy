eaf03f753ae5d92e2b214b4fa50f9b185518e5e3
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-23 16:23:47 +0000

    Fixed

--- src/lib/utils/json.ts
+++ src/lib/utils/json.ts
@@ -2,21 +2,24 @@ export function ToSeriableHome(home){
     return (
         {
             ...home._doc, 
-            _id: home._id.toString(),
-            owner: home.owner.toString(),
-            delegates: home.delegates.map(x => x._id.toString()),
-            createdAt: JSON.stringify(home.createdAt),
-            updatedAt: JSON.stringify(home.updatedAt)
+            _id: (home?._id && home._id.toString()) ?? null,
+            owner: (home?.owner && home?.owner?.toString()) ?? null,
+            delegates: (home?.delegates && home?.delegates.map(x => x._id.toString())) ?? null,
+            createdAt: (home.createdAt && JSON.stringify(home.createdAt)) ?? null,
+            updatedAt: (home.updatedAt && JSON.stringify(home.updatedAt)) ?? null
         }
     )
 }
 
 export function ToSeriableBooking(booking){
+
+    console.log(booking.home)
+
     return (
         {
             ...booking._doc,
             _id: booking._id.toString(),
-            home: booking.home?.toString(),
+            home: booking?.home && ToSeriableHome(booking.home),
             startDateTime: booking.startDateTime?.toString(),
             endDateTime: booking.endDateTime?.toString(),
         }
