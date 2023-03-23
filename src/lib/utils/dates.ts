6608a7ec8b49bd5e10bd266a94ff4fbfcf4edcb8
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-23 14:45:52 +0000

    Added get ordinal

---
+++ src/lib/utils/dates.ts
@@ -0,0 +1,7 @@
+export function getOrdinal(n) {
+    let ord = ["st", "nd", "rd"]
+    let exceptions = [11, 12, 13]
+    let nth = 
+    ord[(n % 10) - 1] == undefined || exceptions.includes(n % 100) ? "th" : ord[(n % 10) - 1]
+    return n + nth
+}
