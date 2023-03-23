93b84ccd5ca4bd5adf07d6badd0214cf42f98bb5
WT000 <55751558+WT000@users.noreply.github.com>
2023-03-23 12:13:16 +0000

    Logos per input

--- src/pages/home/new.tsx
+++ src/pages/home/new.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]";
 import Head from "next/head";
-import { IoHome, IoImages, IoPieChart } from "react-icons/io5";
+import { IoBed, IoFlash, IoFootsteps, IoHome, IoImages, IoPieChart, IoSave, IoText, IoWallet } from "react-icons/io5";
 import Body from "../../components/Body/Body";
 import NavbarMenuItem from "../../components/navbar/NavbarMenuItem/NavbarMenuItem";
 import NavbarStats from "../../components/Stats/Stats";
@@ -11,6 +11,7 @@ import { useSession } from "next-auth/react";
 import InputLayout from "../../components/layouts/InputLayout/InputLayout";
 import Tile from "../../components/Tile/Tile";
 import { TileType } from "../../components/Tile/Tile";
+import Button from "../../components/Button/Button";
 
 const navItems = [
     <NavbarMenuItem key={"allhomes-link"} icon={<IoHome />} text="All Homes" onClick={() => console.log("AllHomes")} activePage={false} />,
@@ -41,11 +42,11 @@ export default function NewHome(props) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Body menuItems={navItems} statItems={statItems} session={userSession}>
-                <h1 className="mx-12 mb-4 text-3xl col-span-1 md:col-span-2">New Home</h1>
+                <h1 className="mx-6 mb-4 text-3xl col-span-1 md:col-span-2">New Home</h1>
 
-                <div className="mx-12 my-2">
+                <div className="mx-6 my-2">
                     {/* To become the form component */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-5">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-24 gap-y-5">
                         {/* Image */}
                         <Tile tileType={TileType.input} clickable={false}>
                             <InputLayout icon={<IoImages size="32px"/>} text={"Home Image"} type={"file"} name={"image"} placeholder={"Select Image"}/>
@@ -53,35 +54,37 @@ export default function NewHome(props) {
 
                         {/* Image Preview */}
                         {/* May need a custom size set on md: breakpoint */}
-                        <Tile tileType={TileType.fill} customClass="row-span-3" clickable={false}>
+                        <Tile tileType={TileType.fill} customClass="row-span-3 p-2" clickable={false}>
                             <p>Image Preview</p>
                         </Tile>
 
                         {/* Name */}
                         <Tile tileType={TileType.input} clickable={false}>
-                            <InputLayout icon={<IoImages size="32px"/>} text={"Home Name"} type={"text"} name={"name"} placeholder={"My New Home..."}/>
+                            <InputLayout icon={<IoText size="32px"/>} text={"Home Name"} type={"text"} name={"name"} placeholder={"My New Home..."}/>
                         </Tile>
 
                         {/* Beds */}
                         <Tile tileType={TileType.input} clickable={false}>
-                            <InputLayout icon={<IoImages size="32px"/>} text={"Number of Beds"} type={"number"} name={"beds"} placeholder={"4"}/>
+                            <InputLayout icon={<IoBed size="32px"/>} text={"Number of Beds"} type={"number"} name={"beds"} placeholder={"4"}/>
                         </Tile>
 
                         {/* Cost Buffer */}
                         <Tile tileType={TileType.input} clickable={false}>
-                            <InputLayout icon={<IoImages size="32px"/>} text={"Cost Buffer"} type={"number"} name={"costbuffer"} placeholder={"2.23"}/>
+                            <InputLayout icon={<IoWallet size="32px"/>} text={"Cost Buffer"} type={"number"} name={"costbuffer"} placeholder={"2.23"}/>
                         </Tile>
 
                         {/* Instructions */}
                         {/* May need a custom size set on md: breakpoint */}
                         <Tile tileType={TileType.fill} customClass="row-span-2" clickable={false}>
-                            <InputLayout icon={<IoImages size="32px"/>} text={"Instructions"} type={"textarea"} name={"instructions"} placeholder={"Enter the room to the left of..."}/>
+                            <InputLayout icon={<IoFootsteps size="32px"/>} text={"Instructions"} type={"textarea"} name={"instructions"} placeholder={"Enter the room to.."}/>
                         </Tile>
 
                         {/* Energy Tariff (per kWh) */}
                         <Tile tileType={TileType.input} clickable={false}>
-                            <InputLayout icon={<IoImages size="32px"/>} text={"Energy Tariff (per Kwh)"} type={"number"} name={"energytariff"} placeholder={"2.23"}/>
+                            <InputLayout icon={<IoFlash size="32px"/>} text={"Energy Tariff (per Kwh)"} type={"number"} name={"energytariff"} placeholder={"2.23"}/>
                         </Tile>
+                        
+                        <Button text="Submit" icon={<IoSave className="text-white"/>}/>
                     </div>
                 </div>
             </Body>
