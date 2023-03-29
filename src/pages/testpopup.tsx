122be51def4df00dba0aa19f7628246189bb9255
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-03-29 12:30:29 +0100

    Cleaned popup test page

--- src/pages/testpopup.tsx
+++ src/pages/testpopup.tsx
@@ -1,10 +1,4 @@
-import NavbarMenu from "../components/navbar/NavbarMenu/NavbarMenu";
-import NavbarMenuItem from "../components/navbar/NavbarMenuItem/NavbarMenuItem";
-import NavbarStats from "../components/Stats/Stats";
-import DesktopNavbar from "../components/navbar/DesktopNavbar/DesktopNavbar";
-import MobileNavbar from "../components/navbar/MobileNavbar/MobileNavbar";
-import MobileHeader from "../components/navbar/MobileHeader/MobileHeader";
-import {IoHome, IoPieChart, IoFlash, IoCalendar, IoList, IoLogOut, IoAdd} from "react-icons/io5";
+
 import Popup from "../components/Popup/Popup";
 
 import ReadingPopupLayout from "../components/layouts/ReadingPopupLayout/ReadingPopupLayout";
@@ -14,27 +8,7 @@ import ReadingPopupLayout from "../components/layouts/ReadingPopupLayout/Reading
 
 
 export default function test() {
-    const navItems = [
-        <NavbarMenuItem
-                        icon={<IoHome />}
-                        text="All Homes"
-                        onClick={() => console.log("AllHomes")}
-                        activePage={false} 
-                    />,
-        <NavbarMenuItem
-                    icon={<IoPieChart />}
-                    text="Dashboard"
-                    onClick={() => console.log("Dashboard")}
-                    activePage={true} 
-        />]
 
-
-
-    const statItems = [
-        <NavbarStats stat="30" text="some stats (testing)" />,
-        <NavbarStats stat="£4.50" text="cost"/>,
-        <NavbarStats stat="60" text="some more stats (that are cool)"/>
-    ]
 
     const popupitems = [
         
@@ -44,43 +18,5 @@ export default function test() {
 
         <Popup delegateName={"Jack"} delegateProfession={"Builder"} date={new Date("2015-05-05T10:23:03.839Z")} distance={5} image={"/stories/popup1.jpg"} name={"image"} children={""}></Popup>
 
-
-
-
-
-
-
-
-        /*<div>
-            <DesktopNavbar 
-            text="Welcome back, Rachel"
-            image="/stories/rachel.jpeg"
-            menu={
-                <NavbarMenu>
-                    {navItems}
-                </NavbarMenu>  
-                }
-            >
-                <NavbarStats stat="30" text="some stats (testing)" />
-                <NavbarStats stat="£4.50" text="cost"/>
-                <NavbarStats stat="60" text="some more stats (that are cool)"/>
-            </DesktopNavbar>
-            
-            <MobileHeader 
-                text="Welcome back, Rachel"
-                image="/stories/rachel.jpeg"
-                currentPage="Homes"
-            />
-        
-            <MobileNavbar 
-                menu={
-                    <NavbarMenu>
-                        {navItems}
-                    </NavbarMenu>
-                } 
-            />
-        
-        
-    </div>*/
     )
 }
