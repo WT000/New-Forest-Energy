ba315cee147f74ef9287bf591aed71da6e12c191
WT000 <55751558+WT000@users.noreply.github.com>
2023-03-30 16:25:14 +0100

    Remove console log

--- src/components/forms/HomeForm/HomeForm.tsx
+++ src/components/forms/HomeForm/HomeForm.tsx
@@ -83,8 +83,6 @@ export default function HomeForm(props: HomeFormProps) {
         }
     }, [triggerReset, reset]);
 
-    console.log(edit?.role)
-
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-24 gap-y-3 mb-14 md:mb-0">
             <Tile tileType={TileType.input} clickable={false}>
