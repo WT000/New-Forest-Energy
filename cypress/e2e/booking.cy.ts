64ce0f659e01931d1ec5a3f916e4488433cfef4c
sophia-attrill1 <5attrs34@solent.ac.uk>
2023-04-28 12:27:53 +0100

    Formatted tests

--- cypress/e2e/booking.cy.ts
+++ cypress/e2e/booking.cy.ts
@@ -1,67 +1,52 @@
 describe("Home Page Test", () => {
-    beforeEach(() => {
-      cy.visit("/");
-      cy.viewport(1920, 1080)
-
-    });
-  
-
-    // it("Date failure check", () => {
-
-    //   cy.getByData('hometile3').eq(0).click({ force: true })
-    //   cy.getByData("navbutton").eq(3).click({ force: true })
-    //   cy.getByData("hometile4").eq(1).click()
-    //   cy.getByData("hometile4").eq(1).type("Adam")
-    //   cy.getByData("testbutton").click()
-    //   cy.get(".text-black-500.text-xs").should("have.text", "Home Guest Surname Start Date *Must be beyond now and not conflict.End Date *Must be beyond start and not conflict.");
-
-
-      
-    // });
-
-    // it("Empty fields validation", () => {
-
-    //     cy.getByData('hometile3').eq(0).click({ force: true })
-    //     cy.getByData("navbutton").eq(3).click({ force: true })
-    //     cy.getByData("hometile4").eq(1).click()
-    //     cy.getByData("testbutton").click()
-    //     cy.get(".text-black-500.text-xs").should("have.text", "Home Guest Surname *RequiredStart Date *Must be beyond now and not conflict.End Date *Must be beyond start and not conflict.");
-  
-  
-        
-    //   });
-
-      it("Generate QR code for booking", () => {
-
-        cy.visit("/bookings/adf7772");
-        cy.getByData("tilebutton").eq(1).click()
-        cy.getByData("deletedtoast").should('be.visible');
-        cy.getByData("deletedtoast").should('have.text', 'Link copied to clipboard.')
-        
-
-        cy.getByData("deletedhomedimiss").click();
-
-        cy.getByData("deletedtoast").should('not.be.visible')
-
-        
-      
-  
-  
-        
-      });
-
-      it("Delete booking", () => {
-
-        cy.visit("/bookings/adf7772");
-        cy.getByData("tilebutton").eq(0).click()
-        cy.url().should("eq", Cypress.config().baseUrl + "/bookings/adf7772/edit");
-        cy.getByData("testbutton").eq(1).click();
-  
-        
-      });
-
-    
-
+  beforeEach(() => {
+    cy.visit("/");
+    cy.viewport(1920, 1080);
   });
-  
-  export {}; 
+
+  it("Date failure check", () => {
+    cy.getByData("hometile3").eq(0).click({ force: true });
+    cy.getByData("navbutton").eq(3).click({ force: true });
+    cy.getByData("hometile4").eq(1).click();
+    cy.getByData("hometile4").eq(1).type("Adam");
+    cy.getByData("testbutton").click();
+    cy.get(".text-black-500.text-xs").should(
+      "have.text",
+      "Home Guest Surname Start Date *Must be beyond now and not conflict.End Date *Must be beyond start and not conflict."
+    );
+  });
+
+  it("Empty fields validation", () => {
+    cy.getByData("hometile3").eq(0).click({ force: true });
+    cy.getByData("navbutton").eq(3).click({ force: true });
+    cy.getByData("hometile4").eq(1).click();
+    cy.getByData("testbutton").click();
+    cy.get(".text-black-500.text-xs").should(
+      "have.text",
+      "Home Guest Surname *RequiredStart Date *Must be beyond now and not conflict.End Date *Must be beyond start and not conflict."
+    );
+  });
+
+  it("Generate QR code for booking", () => {
+    cy.visit("/bookings/adf7772");
+    cy.getByData("tilebutton").eq(1).click();
+    cy.getByData("deletedtoast").should("be.visible");
+    cy.getByData("deletedtoast").should(
+      "have.text",
+      "Link copied to clipboard."
+    );
+
+    cy.getByData("deletedhomedimiss").click();
+
+    cy.getByData("deletedtoast").should("not.be.visible");
+  });
+
+  it("Delete booking", () => {
+    cy.visit("/bookings/adf7772");
+    cy.getByData("tilebutton").eq(0).click();
+    cy.url().should("eq", Cypress.config().baseUrl + "/bookings/adf7772/edit");
+    cy.getByData("testbutton").eq(1).click();
+  });
+});
+
+export {};
--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,84 +1,61 @@
 describe("Home Page Test", () => {
-    beforeEach(() => {
-      cy.visit("/");
+  beforeEach(() => {
+    cy.visit("/");
 
-      cy.viewport(1920, 1080)
-    });
-  
-    it("shows multiple houses", () => {
-
-      cy.getByData("hometile3").should("have.length", 5);
-      cy.getByData("navstats").eq(0).should("have.text", "5  ")
-
-    });
-
-    it("search bar works", () => {
-
-      cy.getByData("hometile4").eq(0).click();
-      cy.getByData("hometile4").type("bob")
-      cy.getByData("hometile3").should("have.length", 1);
-      cy.getByData("hometile3").should("have.length", 1).last().should("have.text","BobsSleeps: 10")
-
-
-    });
-
-    it("navbar stats contain correct stats", () => {
-
-      cy.getByData("navstats").eq(0).should("have.text", "5  ")
-      cy.getByData("navstats").eq(1).should("have.text", "3  ")
-      cy.getByData("navstats").eq(2).should("have.text", "3  ")
-
-    });
-
-    it("delete a home", () => {
-
-      cy.getByData('hometile3').eq(0).trigger('mouseover')
-      cy.getByData('deletehome').eq(0).click({ force: true })
-      cy.getByData("deletedtoast").should('be.visible');
-      cy.getByData("deletedhomedimiss").click();
-      cy.reload()
-      cy.getByData("navstats").eq(0).should("have.text", "4  ")
-      cy.getByData("navstats").eq(0).should("have.text", "4  ")
-      
-
-      
-    });
-
-    it("undo a deleted home", () => {
-
-      cy.getByData('hometile3').eq(0).trigger('mouseover')
-      cy.getByData('deletehome').eq(0).click({ force: true })
-      cy.getByData("deletedtoast").should('be.visible');
-      cy.getByData("deletedhomeundo").should('be.visible');
-      cy.getByData("deletedhomeundo").click();
-      cy.reload()
-      cy.getByData("navstats").eq(0).should("have.text", "5  ")
-      cy.getByData("hometile3").should("have.length", 5);
-
-      
-    });
-
-    it("navigate to create a home page", () => {
-
-      cy.getByData("navbutton").eq(1).click({ force: true })
-      cy.url().should("eq", Cypress.config().baseUrl + "/homes/new")
-      
-    });
-
-    it("navigate to home page", () => {
-
-      cy.getByData("navbutton").eq(0).click({ force: true })
-      cy.url().should("eq", Cypress.config().baseUrl + "/homes")
-      
-    });
-
-
-
-    
-
-    
-  
-
+    cy.viewport(1920, 1080);
   });
-  
-  export {}; 
+
+  it("shows multiple houses", () => {
+    cy.getByData("hometile3").should("have.length", 5);
+    cy.getByData("navstats").eq(0).should("have.text", "5  ");
+  });
+
+  it("search bar works", () => {
+    cy.getByData("hometile4").eq(0).click();
+    cy.getByData("hometile4").type("bob");
+    cy.getByData("hometile3").should("have.length", 1);
+    cy.getByData("hometile3")
+      .should("have.length", 1)
+      .last()
+      .should("have.text", "BobsSleeps: 10");
+  });
+
+  it("navbar stats contain correct stats", () => {
+    cy.getByData("navstats").eq(0).should("have.text", "5  ");
+    cy.getByData("navstats").eq(1).should("have.text", "3  ");
+    cy.getByData("navstats").eq(2).should("have.text", "3  ");
+  });
+
+  it("delete a home", () => {
+    cy.getByData("hometile3").eq(0).trigger("mouseover");
+    cy.getByData("deletehome").eq(0).click({ force: true });
+    cy.getByData("deletedtoast").should("be.visible");
+    cy.getByData("deletedhomedimiss").click();
+    cy.reload();
+    cy.getByData("navstats").eq(0).should("have.text", "4  ");
+    cy.getByData("navstats").eq(0).should("have.text", "4  ");
+  });
+
+  it("undo a deleted home", () => {
+    cy.getByData("hometile3").eq(0).trigger("mouseover");
+    cy.getByData("deletehome").eq(0).click({ force: true });
+    cy.getByData("deletedtoast").should("be.visible");
+    cy.getByData("deletedhomeundo").should("be.visible");
+    cy.getByData("deletedhomeundo").click();
+    cy.reload();
+    cy.getByData("navstats").eq(0).should("have.text", "5  ");
+    cy.getByData("hometile3").should("have.length", 5);
+  });
+
+  it("navigate to create a home page", () => {
+    cy.getByData("navbutton").eq(1).click({ force: true });
+    cy.url().should("eq", Cypress.config().baseUrl + "/homes/new");
+  });
+
+  it("navigate to home page", () => {
+    cy.getByData("navbutton").eq(0).click({ force: true });
+    cy.url().should("eq", Cypress.config().baseUrl + "/homes");
+  });
+});
+
+export {};
